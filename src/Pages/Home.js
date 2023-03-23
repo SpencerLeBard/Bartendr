d895c8bf2828fc6597e8774591991da07ed034c6
SpencerLeBard <slebard@gmail.com>
2023-03-23 09:56:00 -0600

    starting server

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,12 @@ class Home extends React.Component {
     return (
       <div className="Home">
         <Cam />
+        <title>Video Streaming With Node</title>
+        <body>
+          <video id="videoPlayer" width="25%" controls muted="muted" autoplay>
+            <source src="/video" type="video/mp4" />
+          </video>
+        </body>
       </div>
     );
   }
--- src/Server/server.js
+++ src/Server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 const PORT = 3000;
 
 //To define a middleware function, we call app.use() and pass it a function. Here’s a basic middleware function to print the current time in the console during every request:
@@ -18,8 +19,14 @@ app.use("/request-type", (req, res, next) => {
 //req represents the request that was sent to the server; We can use this object to read data about what the client is requesting to do. res represents the response that we will send back to the client.
 //Here, we are calling a function on res to send back a response: 'Successful response'
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Wiki Home Page");
+});
+
+router.get("/about", function (req, res) {
+  res.send("About this wiki");
 });
 
 //Finally, once we’ve set up our requests, we must start our server! We are passing 3000 into the listen function, which tells the app which port to listen on. The function passed in as the second parameter is optional and runs when the server starts up. This provides us some feedback in the console to know that our application is running.
 app.listen(PORT, () => console.log("App is listening on port 3000"));
+
+module.exports = router;
