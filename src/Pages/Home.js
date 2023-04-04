903933f350acdf408973d18e383b4c17f26966c4
SpencerLeBard <slebard@gmail.com>
2023-04-04 09:11:33 -0600

    almost working

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,16 +2,10 @@ import React from "react";
 import LiveStream from "../Server/rtmp/LiveStream";
 
 class Home extends React.Component {
-  //TODO PROBABLY GOING TO HAVE TO DO CONSTRUCTOR PROPS HERE BASED ON ID OF VIDEO
-
   render() {
     return (
       <div className="Home">
         <LiveStream />
-        <title>Video Streaming With Node</title>
-        <video controls muted>
-          <source src="http://localhost:8080/video" type="video/mp4"></source>
-        </video>
       </div>
     );
   }
--- src/Server/rtmp/LiveStream.js
+++ src/Server/rtmp/LiveStream.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import Hls from "hls";
+import Hls from "hls.js";
 
 class LiveStream extends Component {
+  videoRef = React.createRef();
+
   componentDidMount() {
-    const video = document.getElementById("video");
-    const videoSrc = "/hls/test.m3u8";
+    const { current: video } = this.videoRef;
+
     if (Hls.isSupported()) {
       const hls = new Hls();
-      hls.loadSource(videoSrc);
+      hls.loadSource("/hls/test.m3u8");
       hls.attachMedia(video);
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = "/hls/test.m3u8";
     }
   }
 
@@ -16,7 +20,7 @@ class LiveStream extends Component {
     return (
       <div>
         <h2>Live Stream</h2>
-        <video id="video" width="800" controls></video>
+        <video id="video" width="800" controls ref={this.videoRef}></video>
       </div>
     );
   }
