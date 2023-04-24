c989aaea0975cda8a39e8c228ed90e73308ee2af
SpencerLeBard <slebard@gmail.com>
2023-04-24 12:18:32 -0600

    chatgpt integration

--- serverPractice.js
+++ serverPractice.js
@@ -1,34 +1,40 @@
-const express = require("express");
-const app = express();
-const router = express.Router();
-const PORT = 3000;
+const axios = require("axios");
 
-//To define a middleware function, we call app.use() and pass it a function. Here’s a basic middleware function to print the current time in the console during every request:
-app.use((req, res, next) => {
-  console.log("Time: ", Date.now());
-  next();
-});
+const prompt = "Hello, my name is";
+const model = "davinci";
 
-//By passing '/request-type' as the first argument to app.use(), this function will only run for requests sent to localhost:3000/request-type.
-app.use("/request-type", (req, res, next) => {
-  console.log("Request type: ", req.method);
-  next();
-});
+const data = {
+  prompt,
+  model,
+  max_tokens: 5,
+  temperature: 0.5,
+};
 
-//first param is the URL for this function to act upon. We are targeting '/', which is the root of our website: in this case, localhost:3000
-//req represents the request that was sent to the server; We can use this object to read data about what the client is requesting to do. res represents the response that we will send back to the client.
-//Here, we are calling a function on res to send back a response: 'Successful response'
-app.get("/", (req, res) => {
-  res.send("Wiki Home Page");
-});
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+};
 
-router.get("/about", function (req, res) {
-  res.send("About this wiki");
-});
+axios
+  .post("https://api.openai.com/v1/engines/davinci-codex/completions", data, {
+    headers,
+  })
+  .then((response) => {
+    console.log(response.data.choices[0].text);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-//Finally, once we’ve set up our requests, we must start our server! We are passing 3000 into the listen function, which tells the app which port to listen on. The function passed in as the second parameter is optional and runs when the server starts up. This provides us some feedback in the console to know that our application is running.
-app.listen(PORT, () => console.log("App is listening on port 3000"));
+//   String apiKey = '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';
+//   String orgID = 'org-cPUNw2Fgkyun710WHvW6cZ23';
+//   Http http = new Http();
+//   HttpRequest req = new HttpRequest();
+//   req.setEndpoint('https://api.openai.com/v1/chat/completions');
+//   req.setMethod('POST');
+//   req.setHeader('Content-Type', 'application/json');
+//   req.setHeader('Authorization', 'Bearer ' + apiKey);
 
-module.exports = router;
+//   String requestBody = '{"model": "davinci", "prompt": "print hello world in python", "temperature": 0.7, "max_tokens": 64, "n": 1, "stop": ["\\n"] }';
 
-//TODO CALLL TO API AND THEN RENDER ON CLIENT SIDE
+//   req.setBody(requestBody);
